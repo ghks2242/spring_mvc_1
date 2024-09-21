996696b539d6640b798dad0de3563ef6497ba8b7
yunchanghwan <ghks2242@naver.com>
2024-09-21 16:31:20 +0900

    서블릿으로 회원 관리 웹 애플리케이션 만들기 강의 완료

--- src/main/java/hello/servlet/ServletInitializer.java
+++ src/main/java/hello/servlet/ServletInitializer.java
@@ -2,7 +2,7 @@ package hello.servlet;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
+// war 파일로 배포를 진행해야 하는 경우가 있을 수 있다. 이럴 경우 SpringBootServletInitializer를 상속받으면 된다.
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
